c2e7067b91693e39c5d6d66b9583ba9cca62f562
lovis-ambrose <lovisambrose212@gmail.com>
2024-07-04 13:53:09 +0300

    expenses

--- src/app/budget-planner/budget-planner-routing.module.ts
+++ src/app/budget-planner/budget-planner-routing.module.ts
@@ -4,6 +4,7 @@ import { LoginComponent } from './login/login.component';
 import { SideNavComponent } from './side-nav/side-nav.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { IncomeComponent } from './income/income.component';
+import { ExpenseComponent } from './expense/expense.component';
 
 const routes: Routes = [
   {
@@ -17,6 +18,9 @@ const routes: Routes = [
   },
   {
     path: 'income', component: IncomeComponent
+  },
+  {
+    path: 'expense', component: ExpenseComponent
   }
 ];
 
